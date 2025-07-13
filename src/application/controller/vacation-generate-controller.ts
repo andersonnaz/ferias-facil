04a5544921e172dd9f32d00ba3fc192fcddfdad0
andersonnaz <anderson_1004@hotmail.com>
2025-07-13 19:07:18 -0300

    feat: create Vacation Generate Controller

--- src/application/controller/vacation-generate-controller.ts
+++ src/application/controller/vacation-generate-controller.ts
@@ -1,5 +1,5 @@
 import { VacationGenerateImplementation } from '../../data/use-cases/vacation-generate-implementation'
-import { Controller, HttpRequest, HttpResponse, serverError, success } from '../protocols/controller'
+import { Controller, HttpRequest, HttpResponse, notAcceptable, serverError, success } from '../protocols/controller'
 
 export class VacationGenerateController implements Controller {
 	private readonly vacationGenerate: VacationGenerateImplementation
@@ -10,7 +10,12 @@ export class VacationGenerateController implements Controller {
 
 	async handle(HttpRequest: HttpRequest): Promise<HttpResponse> {
 		try {
-			return success('data')
+			const { file } = HttpRequest
+			const result = await this.vacationGenerate.generate(file.path)
+			if (!result.isSuccess) {
+				return notAcceptable(result.error as Error)
+			}
+			return success(result)
 		} catch (error) {
 			return serverError(error as Error)
 		}
