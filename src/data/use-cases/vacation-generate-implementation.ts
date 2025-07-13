55f0c145312c4ccb55703659d7ad3713a7afb69a
andersonnaz <anderson_1004@hotmail.com>
2025-07-13 19:06:03 -0300

    feat: create VacationGenerate usecase implementation

--- src/data/use-cases/vacation-generate-implementation.ts
+++ src/data/use-cases/vacation-generate-implementation.ts
@@ -1,18 +1,23 @@
 import { VacationGenerateUseCase } from '../../domain/use-cases/vacation-generate-use-case'
+import { SheetJsAdapter } from '../../infra/adapters/sheetjs-adapter'
 import { VacationGenerateService } from '../../infra/service/vacation-generate-service'
 
 export class VacationGenerateImplementation implements VacationGenerateUseCase {
 	private readonly vacationGenerateService: VacationGenerateService
+	private readonly parse: SheetJsAdapter
 
-	constructor({ vacationGenerateService }: VacationGenerateUseCase.Dependencies) {
+	constructor({ vacationGenerateService, parse }: VacationGenerateUseCase.Dependencies) {
 		this.vacationGenerateService = vacationGenerateService
+		this.parse = parse
 	}
 
-	generate(params: VacationGenerateUseCase.Params): VacationGenerateUseCase.Result {
-		const vacationGenerateResult = this.vacationGenerateService.generate(params)
+	async generate(filePath: VacationGenerateUseCase.Params): Promise<VacationGenerateUseCase.Result> {
+		const vacationList = await this.parse.toJson(filePath)
+		const vacationGenerateResult = this.vacationGenerateService.generate(vacationList)
+		const vacationListExportedToExcel = await this.parse.toExcel({ filePath, data: vacationGenerateResult })
 		return {
 			isSuccess: true,
-			data: vacationGenerateResult
+			data: vacationListExportedToExcel
 		}
 	}
 }
